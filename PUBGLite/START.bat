ca13ab16cb0f1489798d7121f91c044127f05b8c
Dicer-TDP <51524891+Dicer-TDP@users.noreply.github.com>
2020-06-23 10:30:42 +0700

    Create START.bat

---
+++ PUBGLite/START.bat
@@ -0,0 +1,24 @@
+:prompt
+title X-Dicer-Byte
+cls
+@echo off
+echo  X         X       DDD
+echo   X       X        D  DD
+echo    X     X         D    DD
+echo     X   X          D      DD
+echo      X X           D        DD   
+echo       XX      ==== D          DD
+echo      X  X          D          DD
+echo     X    X         D        DD
+echo    X      X        D      DD
+echo   X        X       D    DD
+echo  X          X      DDDDD
+echo  Develop by: Dicer-TDP
+echo  Contact me: akbar.rempeyek21@gmail.com
+color ac
+cd ..
+cd ..
+D:
+cd Online Game/PUBGLite && Launcher.exe
+@cls
+pause 5 && exit
